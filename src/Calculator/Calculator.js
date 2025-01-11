86a4b6694f2b3b54dbaa9ca401585241acb89983
DinosMpo <dinos7591@yahoo.gr>
2025-01-11 12:33:16 +0200

    part 7

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 import './Calculator.css';
 import { numbers, operators } from '../data';
 
 export default function Calculator() {
+    const [value, setValue] = useState(0);
+    const [output, setOutput] = useState("");
+
+    // const activeNumber = 0;
+
     const numbersList = numbers.map((num, key) => {
         return <div id={num.name} className="number" key={key}>{num.text}</div>
     });
@@ -10,9 +16,25 @@ export default function Calculator() {
         return <div id={operator.name} className="operator" key={key}>{operator.text}</div>
     });
 
+    const clear = () => {
+        setValue(0);
+        setOutput("");
+    }
+
     return (
         <div id="calculator">
-            <div id="display"></div>
+            <div id="display">
+                <div id="output">
+                    <div className="value">{value}</div>
+                </div>
+                <input id="input" type="text" value={value} onChange={(e) => {
+                    if(value === 0) {
+                        let removeZero = e.target.value.replace(0, ''); 
+                        e.target.value = removeZero;
+                    }
+                    setValue(e.target.value);
+                }} />
+            </div>
             <div id="container">
                 <div id='wrapper'>
                     <div id="numbers-container">
@@ -22,7 +44,7 @@ export default function Calculator() {
                     </div>
                     <div id="operators-container">{operatorsList}</div>
                 </div>
-                <div id="clear">Clear</div>
+                <div id="clear" onClick={() => clear()}>Clear</div>
             </div>
         </div>
     )
